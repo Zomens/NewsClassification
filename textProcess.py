9a31a807cd2ddcdf312df5b6021a59575195a9a3
Zomens <zhoumansuo@qq.com>
2019-06-17 20:16:18 +0800

    增加遍历目录代码

--- textProcess.py
+++ textProcess.py
@@ -6,6 +6,7 @@ Created on Mon Jun 17 18:21:43 2019
 """
 import jieba
 import json
+import os
 
 def createStopWordDict():
     stopWordDict = {}
@@ -23,6 +24,32 @@ def readStopWordDict():
         stopWordDict = json.load(file)
     return stopWordDict
 
+def cutWord(path, stopWordDict):
+    text = ""
+    with open(path, "r", encoding="utf-8") as lines:
+        for line in lines:
+            line = line.strip()
+            text = text + " " + line
+    cutList = jieba.cut(text)
+    segList = []
+    for word in cutList:
+        if word not in stopWordDict:
+            segList.append(word)
+    return segList
+        
+    
+
 if __name__ == "__main__":
 #    createStopWordDict()
+    newsPath = "../THUCNews/体育"
     stopWordDict = readStopWordDict()
+    for root, dirNames, files in os.walk(newsPath):
+        for fileName in files:
+            path = os.path.join(root, fileName)
+#            print(path)
+            label = "__sports__"
+            textCutWord = " ".join(cutWord(path, stopWordDict))
+            textCutWord = textCutWord + "    \t" + label
+            with open("sports.txt", "a+", encoding="utf-8") as textSingel:
+                textSingel.write(textCutWord + "\n")
+            
